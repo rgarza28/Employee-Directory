693e749db8e663d7546f74e262d25de60841db5c
Robert Garza <garza.robert28@gmail.com>
2020-08-13 21:14:58 -0500

    Updates

---
+++ precache-manifest.98bcf2ec5d60404d1b6f674fd45b7c7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bd23559a53ae7814421983945bd4904",
+    "url": "/Employee-Directory/index.html"
+  },
+  {
+    "revision": "38a053a0077630551634",
+    "url": "/Employee-Directory/static/css/main.78efbea1.chunk.css"
+  },
+  {
+    "revision": "a369505eb373bcb31daf",
+    "url": "/Employee-Directory/static/js/2.5e724852.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Employee-Directory/static/js/2.5e724852.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38a053a0077630551634",
+    "url": "/Employee-Directory/static/js/main.cbaf2ea1.chunk.js"
+  },
+  {
+    "revision": "21ab4dbde2f8e2bfdf72",
+    "url": "/Employee-Directory/static/js/runtime-main.965eecad.js"
+  }
+]);
